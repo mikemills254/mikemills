40c37010bc6a644c27b5a7643536579c9a473f22
mikemills254 <mikemills930@gmail.com>
2023-05-29 13:28:24 +0300

    First Commit

---
+++ src/content_option.js
@@ -0,0 +1,116 @@
+const logotext = "MIKE MILLS";
+const meta = {
+    title: "Mike Mills",
+    description: "I’m Mike Mills data scientist _ Full stack developer,currently working in Eldoret",
+};
+
+const introdata = {
+    title: "I'm Mike Mills",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop native mobile apps",
+    },
+    description: "I am an experienced software developer specializing in native mobile application development. With two years of experience as a junior developer, I possess a strong skill set and a passion for creating innovative and user-friendly mobile applications.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "I am a seasoned software developer with expertise in crafting high-quality native mobile applications. Over the course of two years as a junior developer, I have honed my skills and cultivated a deep passion for delivering innovative and user-centric mobile experiences.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "React Native",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Html",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Scraping",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.etsy.com/listing/1169019959/star-rating-outline-svg-star-rating",
+        description: "Star rating system with Html CSS and Javascript",
+        link: "https://github.com/mikemills254/Rating-Star-",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Create a server on your local network and be able to communicate using a client to the server with no redundancy. It can also be done on another machine found in the same local network.",
+        link: "https://github.com/mikemills254/TCP-Socket-Programming-Python",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mikemills930@gmail.com",
+    YOUR_FONE: "+254 701 233 944",
+    description: "Feel free to reach out to me! I'm always excited to connect and discuss potential collaborations, projects, or simply have a conversation. Whether you have a question, an idea, or just want to say hello, I'm just a message away",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/mikemills254",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/michael-mills-4774a01a5/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/home/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Typewriter from "typewriter-effect";
+import { introdata, meta } from "../../content_option";
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+  return (
+    <HelmetProvider>
+      <section id="home" className="home">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <div className="intro_sec d-block d-lg-flex align-items-center ">
+          <div
+            className="h_bg-image order-1 order-lg-2 h-100 "
+            style={{ backgroundImage: `url(${introdata.your_img_url})` }}
+          ></div>
+          <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+            <div className="align-self-center ">
+              <div className="intro mx-auto">
+                <h2 className="mb-1x" style={{color: '#00a97f', fontSize: 50, fontWeight: 600}}>{introdata.title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        introdata.animated.first,
+                        introdata.animated.second,
+                        introdata.animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 10,
+                    }}
+                  />
+                </h1>
+                <p className="mb-1x">{introdata.description}</p>
+                <div className="intro_btn-action pb-5">
+                  <Link to="/portfolio" className="text_2">
+                    <div className="btn portfolio_btn">
+                      <span>Portfolio</span>
+                      <svg viewBox="0 0 13 10" height="10px" width="15px">
+                        <path d="M1,5 L11,5"></path>
+                        <polyline points="8 1 12 5 8 9"></polyline>
+                      </svg>
+                    </div>
+                  </Link>
+                  <Link to="/contact">
+                    <div className="contact_btn btn" id="button_h">
+                      Contact Me
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </HelmetProvider>
+  );
+};
